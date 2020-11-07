22991be1f1ae933e9a394a209e3cd31648c75026
bevoosa <eevvoossaa@gmail.com>
2020-11-07 11:21:27 +0200

    removed support for short words

--- src/song_analyzer/song_parts/word.py
+++ src/song_analyzer/song_parts/word.py
@@ -9,8 +9,13 @@ class Word:
         self.data = word_data.lower()
         self.pos = self.get_pos()
 
-    def get_pos(self):
+    def get_pos(self) -> str:
         """
         get the word's part of speech
+        currently, there is no support for short words, such as "we'll" or "you've"
+        :return: the word's POS, or an empty string if it's a shortened word
         """
-        return TextBlob(self.data).tags[0][1]
+        if "'" not in self.data:
+            return TextBlob(self.data).tags[0][1]
+        else:
+            return ''
--- src/song_analyzer/utils/histogram_utils.py
+++ src/song_analyzer/utils/histogram_utils.py
@@ -1,14 +1,17 @@
 import os
 from song_analyzer.song_parts.song import Song
-from song_analyzer.utils.song_utils import
+
 # TEST
 lyrics_dir = r"C:\Users\evoosa\Desktop\JobShit\SongAnalyzer\lyrics"
 lyrics_files = [os.path.join(lyrics_dir, f) for f in os.listdir(lyrics_dir)]
-print(lyrics_files)
 
 song_objects = [Song(lyrics) for lyrics in lyrics_files]
-map(lambda song: song.get_words_pos(), song_objects)
+for s in song_objects:
+    for sen in s.sentences:
+        for w in sen.words:
+            print(w.data, w.pos)
 
-songs_noun_to_adj_ratio = []
+noun_to_adj_ratios = [song.noun_to_adj_ratio for song in song_objects]
+print(noun_to_adj_ratios)
 # get the frequency of the ratios
 # print nicely
