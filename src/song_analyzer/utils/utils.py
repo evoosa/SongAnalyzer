e87d0f21fab899dc468e67f45543f4198fa6c370
bevoosa <eva.osherovsky@stargo.co>
2020-11-07 19:00:13 +0200

    sepearted get len stats to file

--- src/song_analyzer/utils/utils.py
+++ src/song_analyzer/utils/utils.py
@@ -1,4 +1,5 @@
 from song_analyzer.song_parts.song import Song
+from song_analyzer.utils.song_stats_utils import get_sentence_length_stats, get_word_length_stats
 
 
 def get_song_pos_entities(lyrics_path: str):
@@ -8,3 +9,14 @@ def get_song_pos_entities(lyrics_path: str):
         'adjectives': song_obj.adjectives,
         'noun_to_adjective_ratio': song_obj.noun_to_adj_ratio
     }
+
+def get_song_length_statistics(lyrics_path: str):
+    song_obj = Song(lyrics_path)
+    sentence_stats = get_sentence_length_stats(song_obj)
+    words_stats = get_word_length_stats(song_obj)
+    return {
+        'sentence_stats': sentence_stats,
+        'word_stats': words_stats
+    }
+
+
