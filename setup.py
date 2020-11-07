2e12473f61a084964d3763799819cae63a744808
bevoosa <eevvoossaa@gmail.com>
2020-11-07 11:53:17 +0200

    curr state

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='1.0.0',
     install_requires=[
         'textblob==0.15.3',
-        #'textblob.download_corpora' # FIXME ? its a module?
+        'pandas==1.1.14,'
+        'matplotlib==3.3.1'
+        # 'textblob.download_corpora' # FIXME ? its a module?
     ],
     author='evoosa',
     description='feed it with lyrics and get some valuable data about it'
--- src/song_analyzer/utils/histogram_utils.py
+++ src/song_analyzer/utils/histogram_utils.py
@@ -1,17 +1,31 @@
 import os
 from song_analyzer.song_parts.song import Song
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 # TEST
 lyrics_dir = r"C:\Users\evoosa\Desktop\JobShit\SongAnalyzer\lyrics"
 lyrics_files = [os.path.join(lyrics_dir, f) for f in os.listdir(lyrics_dir)]
 
 song_objects = [Song(lyrics) for lyrics in lyrics_files]
-for s in song_objects:
-    for sen in s.sentences:
-        for w in sen.words:
-            print(w.data, w.pos)
 
-noun_to_adj_ratios = [song.noun_to_adj_ratio for song in song_objects]
-print(noun_to_adj_ratios)
-# get the frequency of the ratios
-# print nicely
+# for s in song_objects:
+#     for sen in s.sentences:
+#         for w in sen.words:
+#             print(w.data, w.pos)
+
+noun_adj_ratios = [song.noun_to_adj_ratio for song in song_objects]
+print(noun_adj_ratios)
+
+noun_adj_ratios_np = pd.Series(noun_adj_ratios)
+noun_adj_ratios_np.plot.hist(grid=True, bins='auto', rwidth=0.9,
+                   color='#607c8e')
+plt.title('Commute Times for 1,000 Commuters')
+plt.xlabel('noun/adjective ratio')
+plt.ylabel('number of songs')
+plt.grid(axis='y', alpha=0.75)
+
+# TODO - one for all artists, one for specific artists
