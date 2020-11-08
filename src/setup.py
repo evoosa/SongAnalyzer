56f2d1922c4a42e93df89ad192c1301d631200b2
bevoosa <eva.osherovsky@stargo.co>
2020-11-08 13:17:35 +0200

    changed setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,10 @@ setuptools.setup(
     name='song_analyzer',
     version='1.0.0',
     install_requires=[
-        'textblob==0.15.3',
         'pandas==1.1.4',
         'matplotlib==3.3.1',
-        'numpy==1.19.3'
-        # 'textblob.download_corpora' # FIXME ? its a module?
+        'numpy==1.19.3',
+        'nltk==3.5',
     ],
     author='evoosa',
     description='feed it with lyrics and get some valuable data about it'
