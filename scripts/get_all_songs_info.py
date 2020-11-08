fb32cf16c9278d43101a349f41a3dd363a2e1d92
bevoosa <eva.osherovsky@stargo.co>
2020-11-08 17:21:52 +0200

    prettified all script

--- scripts/get_all_songs_info.py
+++ scripts/get_all_songs_info.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
 
     print('\n[ Getting all songs info.. ]\n')
     song_objects = [Song(lyrics) for lyrics in LYRICS_FILE_PATHS]
-
     artists = {}
     for song_obj in song_objects:
         artist_name = song_obj.metadata['artist']
@@ -18,21 +17,14 @@ if __name__ == '__main__':
         create_dir_if_missing(artist_output_dir)
 
         if artist_name not in artists.keys():
-            artists[artist_name] = {
-                'songs': [],
-                'output_dir': artist_output_dir
-            }
-
+            artists[artist_name] = dict(songs=[], output_dir=artist_output_dir)
             create_gen_stats_csv(os.path.join(artist_output_dir, 'general_statistics.csv'))
 
         artists[artist_name]['songs'].append(song_obj)
-
         print('\n[[ {} ]]'.format(song_obj.metadata['name']))
-
-        print('[ writing POS entities ]')
+        print('[ updating POS entities ]')
         update_pos_stats_file(os.path.join(artist_output_dir, 'pos_statistics.txt'), song_obj)
-
-        print('[ writing general stats ]')
+        print('[ updating general stats ]')
         update_general_stats_file(os.path.join(artist_output_dir, 'general_statistics.csv'), song_obj)
 
     print('\n[[ Create POS histograms for all artists ]]')
